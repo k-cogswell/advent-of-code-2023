5e9315f46b23c9abf8fc7701c0b9e624daa576d3
Kyle Cogswell <kcogswell26@gmail.com>
2024-01-03 14:59:03 -0400

    save progess on part 2

--- challenges/day-01/index.ts
+++ challenges/day-01/index.ts
@@ -2,48 +2,67 @@ import { readFileSync } from 'fs'
 import path from 'path'
 
 const input = readFileSync(path.resolve(__dirname, './input.txt')).toString();
-
+// const input = `
+// 1abc2
+// pqr3stu8vwx
+// a1b2c3d4e5f
+// treb7uchet`
+/**
+ * https://adventofcode.com/2023/day/1#part1
+ * 
+ * @param input data from challenge
+ */
 function partOne(input:string) {
-	let sum = 0;
+	let sum: number = 0;
 
-	input.split(/\r?\n/).forEach(line => {
-		let first = line.match(/(?=.*\d)(\d)/g)
-		let last = line.match(/(\d)(?!.*\d)/g)
-	
-		if (first && last) {
-			sum += parseInt((first[0] + last[0]));
+	input.split(/\r?\n/).forEach(line => {		
+		const match: RegExpMatchArray|null = line.match(/(\d)/g)
+		if (match) {
+			sum += parseInt((match[0] + match[match.length - 1]));
 		}
 	})
 	
 	console.log('part one total', sum)
 }
 
-partOne(input)
+// partOne(input)
 
+/**
+ * https://adventofcode.com/2023/day/1#part2
+ * 
+ * @param input data from challenge
+ */
 function partTwo(input:string) {
-	let sum = 0;
-	const wordMap = {
-		'one': 1,
-		'two': 2,
-		'three': 3,
-		'four': 4,
-		'five': 5,
-		'six': 6,
-		'seven': 7,
-		'eight': 8,
-		'nine': 9
+	let sum: number = 0;
+	const wordDictionary: Record<string, string> = {
+		'one': '1',
+		'two': '2',
+		'three': '3',
+		'four': '4',
+		'five': '5',
+		'six': '6',
+		'seven': '7',
+		'eight': '8',
+		'nine': '9'
 	}
 
-	const matches = '1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine';
+	const matches: string = '1|2|3|4|5|6|7|8|9|one|two|three|four|five|six|seven|eight|nine';
 
 	input.split(/\r?\n/).forEach(line => {
-		let first = line.match(/(?=.*((?:\d*\.)?\d+))((?:\d*\.)?\d)/g)
-		let last = line.match(/((?:\d*\.)?\d)(?!.*((?:\d*\.)?\d+))/g)
-	
-		if (first && last) {
-			sum += parseInt((first[0] + last[0]));
+		console.log(line)
+		const match: RegExpMatchArray|null = line.match(new RegExp(`(${matches})`, "g"))
+
+		if (match) {
+			const len: number = match.length - 1;
+			const first: string = Number.isNaN(parseInt(match[0])) ? wordDictionary[match[0]] : match[0];
+			const last: string = Number.isNaN(parseInt(match[len])) ? wordDictionary[match[len]] : match[len];			
+			console.log(`${first}${last}`)
+			sum += parseInt(`${first}${last}`);
 		}
+		console.log('------------------')
 	})
 	
-	console.log('part one total', sum)
+	console.log('part two total', sum)
 }
+
+partTwo(input)
