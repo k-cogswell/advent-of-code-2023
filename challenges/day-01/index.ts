bd165ff856d59d416733c57bf61d17a831810173
Kyle Cogswell <kcogswell26@gmail.com>
2024-01-05 08:35:03 -0400

    Complete day 2 part 2

--- challenges/day-01/index.ts
+++ challenges/day-01/index.ts
@@ -8,13 +8,13 @@ const input = getInput(__dirname);
  * @param input data from challenge
  */
 function partOne(input:string) {
-	let sum: number = 0;
+	let sum: number = 0
 
 	input.split(/\r?\n/).forEach(line => {		
 		const match: RegExpMatchArray|null = line.match(/(\d)/g)
     if (!match) return
 
-		sum += parseInt((match[0] + match[match.length - 1]));
+		sum += parseInt((match[0] + match[match.length - 1]))
 	})
 	
 	console.log('part one total', sum)
@@ -28,8 +28,8 @@ partOne(input)
  * @param input data from challenge
  */
 function partTwo(input:string) {
-	let sum: number = 0;
-	const pattern: string = '(?=(one|two|three|four|five|six|seven|eight|nine|\\d))';
+	let sum: number = 0
+	const pattern: string = '(?=(one|two|three|four|five|six|seven|eight|nine|\\d))'
 	const numberTranslationDictionary: Record<string, string> = {
 		'one': '1',
 		'two': '2',
@@ -43,18 +43,18 @@ function partTwo(input:string) {
 	}
 
 	input.split(/\r?\n/).forEach(line => {
-		let digits: Array<string> = [];
+		let digits: Array<string> = []
 		const matches: IterableIterator<RegExpMatchArray>|null = line.matchAll(new RegExp(`${pattern}`, "g"))
 
 		for (const match of matches) {
-			digits.push(match[1]);
+			digits.push(match[1])
 		}
 
-		const len: number = digits.length - 1;
-		const first: string = Number.isNaN(parseInt(digits[0])) ? numberTranslationDictionary[digits[0]] : digits[0];
-		const last: string = Number.isNaN(parseInt(digits[len])) ? numberTranslationDictionary[digits[len]] : digits[len];		
+		const len: number = digits.length - 1
+		const first: string = Number.isNaN(parseInt(digits[0])) ? numberTranslationDictionary[digits[0]] : digits[0]
+		const last: string = Number.isNaN(parseInt(digits[len])) ? numberTranslationDictionary[digits[len]] : digits[len]		
 			
-		sum += parseInt(`${first}${last}`);
+		sum += parseInt(`${first}${last}`)
 	})
 	
 	console.log('part two total', sum)
--- challenges/day-02/index.ts
+++ challenges/day-02/index.ts
@@ -6,8 +6,8 @@ const input = getInput(__dirname);
  * 
  * @param input data from challenge
  */
-function partOne(input:string) {
-	let sum: number = 0;
+function partOne(input: string) {
+	let sum: number = 0
 	const colorLimits: Record<string, number> = {
 		"red": 12,
 		"green": 13,
@@ -15,7 +15,7 @@ function partOne(input:string) {
 	}
 
 	input.split(/\r?\n/).forEach(line => {		
-		let gameIsValid: boolean = true;
+		let gameIsValid: boolean = true
 		const matches: IterableIterator<RegExpMatchArray>|null = line.matchAll(/\d+(?= (red|green|blue))/g)
 
 		if (!matches) return
@@ -29,7 +29,7 @@ function partOne(input:string) {
 
 		if (!gameIsValid) return 
 
-		const gameID: RegExpMatchArray | null = line.match(/\d+(?=:)/g)
+		const gameID: RegExpMatchArray|null = line.match(/\d+(?=:)/g)
 
 		if (!gameID) return
 		
@@ -40,3 +40,31 @@ function partOne(input:string) {
 }
 
 partOne(input)
+
+/**
+ * https://adventofcode.com/2023/day/2#part2
+ * 
+ * @param input data from challenge
+ */
+function partTwo(input: string) {
+	let sum: number = 0
+	input.split(/\r?\n/).forEach(line => {
+		const tally: Record<string, number> = {
+			"red": 0,
+			"green": 0,
+			"blue": 0
+		}
+		const matches: IterableIterator<RegExpMatchArray>|null = line.matchAll(/\d+(?= (red|green|blue))/g)
+
+		for (const match of matches) {
+			if (parseInt(match[0]) <= tally[match[1]]) continue
+			tally[match[1]] = parseInt(match[0]);
+		}
+
+		sum += tally["red"] * tally["blue"] * tally["green"]
+	})
+	
+	console.log('part two total', sum)
+}
+
+partTwo(input)
