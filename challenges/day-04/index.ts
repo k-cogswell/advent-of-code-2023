db540cc23b5058199c81d0f1fa12f6aa5a5b058d
Kyle Cogswell <kcogswell26@gmail.com>
2024-01-08 11:46:29 -0400

    Complete day 4 part 1

---
+++ challenges/day-04/index.ts
@@ -0,0 +1,44 @@
+import getInput from "../../utils/getInput";
+const input = getInput(__dirname);
+// const input = `Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+// Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+// Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+// Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+// Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+// Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11`
+
+/**
+ * https://adventofcode.com/2023/day/3#part1
+ * 
+ * @param input data from challenge
+ */
+function partOne(input: string) {
+  let sum: number = 0
+
+  const winners = input
+    .split('\n')
+    .map(line => {
+      let [left, right] = line
+        .split("|")
+        .map((group, i) => group.match(/(\d+)/g))
+        
+      const numberOfWinners = left?.slice(1).filter(num => right?.includes(num)).length || 0
+      return numberOfWinners > 0 ? Math.pow(2, numberOfWinners - 1) : 0
+    })
+    .reduce((a,b) => a + b)
+
+  sum = winners
+  console.log('Part one total', sum)
+}
+
+partOne(input)
+
+/**
+ * https://adventofcode.com/2023/day/3#part2
+ * 
+ * @param input data from challenge
+ */
+function partTwo(input: string) {
+}
+
+partTwo(input)
